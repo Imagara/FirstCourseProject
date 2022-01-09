f4d590e10ee0b92ce7adc1947f53f16791defdb8
Артем Гавриленко <milick33@gmail.com>
2022-01-09 19:26:49 +0700

    /

---
+++ Kusach/TestPGPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kusach
+{
+    /// <summary>
+    /// Логика взаимодействия для TestPGPage.xaml
+    /// </summary>
+    public partial class TestPGPage : Page
+    {
+        public TestPGPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
